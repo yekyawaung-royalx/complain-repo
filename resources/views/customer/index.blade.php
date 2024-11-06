726bcb3cbbb2783598599dd3d76ce8a6f74ac974
thawzinaung <thawzinmm1999@gmail.com>
2024-11-06 10:00:53 +0630

    stup 1

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -295,7 +295,7 @@
     <script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit" async defer></script>
     <script type="text/javascript">
         var onloadCallback = function() {
-            alert("grecaptcha is ready!");
+            // alert("grecaptcha is ready!");
         };
     </script>
 </body>
