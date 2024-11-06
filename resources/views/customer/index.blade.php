6418f4df51a1bafbfcea3cb25cf5a3f8590a2b3a
thawzinaung <thawzinmm1999@gmail.com>
2024-11-06 10:33:16 +0630

    ste

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -113,12 +113,13 @@
                                     <input class="form-control form-control-lg" id="image" type="file"
                                         name="image">
                                 </div>
-                                <p><strong>* is required</strong></p>
+
                             </div>
                             <div class="mb-3">
                                 <div class="g-recaptcha" data-sitekey="6LfLgnYqAAAAABbjSDHdYP9eUCD84AHkjSAjGqdk">
                                 </div>
                             </div>
+                            <p><strong>* is required</strong></p>
                             <div class="col-lg-3">
                                 <div class="form-group p-1">
                                     <button class="btn btn-danger" id="customer-submit"
