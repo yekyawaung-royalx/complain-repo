68c1611b82c06d6f59329c65055589bf1b54a888
thawzinaung <thawzinmm1999@gmail.com>
2024-11-09 11:35:02 +0630

    sidebar

--- resources/views/dashboard2.blade.php
+++ resources/views/dashboard2.blade.php
@@ -9,7 +9,7 @@
                     </div>
                     <div class="col-12 col-sm-6">
                         <ol class="breadcrumb">
-                            <li class="breadcrumb-item"> <a class="home-item" href="index.html"><i data-feather="home"></i></a>
+                            <li class="breadcrumb-item"> <a class="home-item"><i data-feather="home"></i></a>
                             </li>
                             <li class="breadcrumb-item"> Dashboard</li>
                             <li class="breadcrumb-item active"> Default</li>
