8935f0dcec5b63df3d4b0ced9fde4f17c88b6c3d
thawzinaung <thawzinmm1999@gmail.com>
2024-11-09 11:07:12 +0630

    side bar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -153,7 +153,7 @@
                             </ul>
                         </li>
                     @endif
-                    <li class="sidebar-list"><a class="sidebar-link sidebar-title" href="#">
+                    {{-- <li class="sidebar-list"><a class="sidebar-link sidebar-title" href="#">
                             <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
                                 xmlns="http://www.w3.org/2000/svg">
                                 <g>
@@ -175,7 +175,7 @@
                             <li><a href="#">Branches</a></li>
                             <li><a href="#">Employees</a></li>
                         </ul>
-                    </li>
+                    </li> --}}
                     <li class="sidebar-list"><a class="sidebar-link sidebar-title link-nav"
                             href="support-ticket.html">
                             <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
