dd58a5028c83be7d174a2520719ec0c649db7667
thawzinaung <thawzinmm1999@gmail.com>
2024-11-13 11:35:16 +0630

    update

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,19 +1,36 @@
 @extends('layouts.app1')
 @section('content')
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <style>
+        .page-wrapper .page-body-wrapper .page-title .breadcrumb {
+            justify-content: flex-start;
+        }
+    </style>
     <div class="page-body">
         <div class="container-fluid">
             <div class="page-title">
                 <div class="row">
-                    <div class="col-12 col-sm-6">
-                        <h3>CX Team</h3>
-                    </div>
                     <div class="col-12 col-sm-6">
                         <ol class="breadcrumb">
                             <li class="breadcrumb-item"> <a class="home-item" href="index.html">Dashboard</a>
                             </li>
                             <li class="breadcrumb-item active"> CX Team</li>
                         </ol>
+                    </div>
+                    <div class="col-12 col-sm-6">
+                        <div class="mb-1 breadcrumb-right" style="text-align: end">
+                            <div class="btn-group" role="group" aria-label="Basic example">
+                                <button type="button"
+                                    class="btn btn-primary waves-effect waves-float waves-light pagination-btn"
+                                    id="prev-btn"><i data-feather='skip-back'></i></button>
+                                <button type="button"
+                                    class="btn btn-primary waves-effect waves-float waves-light pagination-btn"
+                                    id="next-btn"><i data-feather='skip-forward'></i></button>
+                                <button type="button"
+                                    class="btn btn-outline-primary waves-effect waves-float waves-light">Records: <span
+                                        id="to-records">0</span> of <span id="total-records">0</span></button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -98,19 +115,6 @@
 
                                     </tbody>
                                 </table>
-                            </div>
-                            <div class="mb-1 breadcrumb-right text-center">
-                                <div class="btn-group" role="group" aria-label="Basic example">
-                                    <button type="button"
-                                        class="btn btn-primary waves-effect waves-float waves-light pagination-btn"
-                                        id="prev-btn"><i data-feather='skip-back'></i></button>
-                                    <button type="button"
-                                        class="btn btn-primary waves-effect waves-float waves-light pagination-btn"
-                                        id="next-btn"><i data-feather='skip-forward'></i></button>
-                                    <button type="button"
-                                        class="btn btn-outline-primary waves-effect waves-float waves-light">Records: <span
-                                            id="to-records">0</span> of <span id="total-records">0</span></button>
-                                </div>
                             </div>
                         </div>
                     </div>
