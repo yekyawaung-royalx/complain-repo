458465331cdbc8733495eb9d6e4efc6e1806022b
thawzinaung <thawzinmm1999@gmail.com>
2024-11-27 11:55:02 +0630

    update

--- resources/views/complaints/view.blade.php
+++ resources/views/complaints/view.blade.php
@@ -8,6 +8,15 @@
         padding: 0 6px;
         float: left;
         width: 33%;
+        transition: transform .2s;
+    }
+
+    .responsive:hover {
+        -ms-transform: scale(1.5);
+        /* IE 9 */
+        -webkit-transform: scale(1.5);
+        /* Safari 3-8 */
+        transform: scale(1.5);
     }
 </style>
 @section('content')
